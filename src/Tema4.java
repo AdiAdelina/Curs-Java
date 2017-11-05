c8207cb246c2e23612386b7b1e64c62b68eb432c
AdiAdelina <iorgaadelina@yahoo.com>
2017-11-05 14:22:01 +0200

    update
    if it's stupid but it works, it's not stupid =_=

--- src/Tema4.java
+++ src/Tema4.java
@@ -1,14 +1,68 @@
-import java.util.*;
-import java.text.*;
+import java.util.Scanner;
 
 public class Tema4 {
+    public static void main(String[] args){
 
-    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        String luna;
+        int zi;
+        int an;
 
+        System.out.println("Introduceti o data " );
 
+        System.out.println("Introduceti ziua ");
+        zi=sc.nextInt();
 
+        System.out.println("Introduceti numele complet al lunii" );
+        luna=sc.next();
 
+        System.out.println("Introduceti anul de forma yyyy" );
+        an=sc.nextInt();
+
+        System.out.println("Data introdusa este: " + zi + luna + an);
+
+        if (an > 9999 && zi > 31 )
+            System.out.println("Data introdusa nu este valida");
+        else if (luna.equals("Ianuarie") || luna.equals("ianuarie"))
+            System.out.println("Data introdusa este valida " + zi +luna+an);
+        else if (luna.equals("Martie") || luna.equals("martie"))
+              System.out.println("Data introdusa este valida " + zi +luna+an);
+        else if (luna.equals("Mai") || luna.equals("mai"))
+            System.out.println("Data introdusa este valida " + zi +luna+an);
+        else if (luna.equals("Iulie") || luna.equals("iulie"))
+            System.out.println("Data introdusa este valida " + zi +luna+an);
+        else if (luna.equals("August") ||luna.equals("august"))
+            System.out.println("Data introdusa este valida " + zi +luna+an);
+        else if (luna.equals("Octombrie") || luna.equals("octombrie"))
+            System.out.println("Data introdusa este valida " + zi +luna+an);
+        else if (luna.equals("Decembrie") || luna.equals("decembrie"))
+            System.out.println("Data introdusa este valida " + zi +luna+an);
+
+        else if (luna.equals("Februarie") || luna.equals("februarie") && ((an % 4 == 0) && (an % 100 != 0) || (an % 400 == 0)) && zi>29)
+            System.out.println("Data introdusa nu este valida");
+        else if (luna.equals("Februarie") || luna.equals("februarie") && ((an % 4 == 0) && (an % 100 != 0) || (an % 400 == 0)) && zi<30)
+            System.out.println("Data introdusa este valida " + zi +luna+an);
+        else if (luna.equals("Februarie") || luna.equals("februarie") && !((an % 4 == 0) && (an % 100 != 0) || (an % 400 == 0)) && zi>28)
+            System.out.println("Data introdusa nu este valida");
+        else if (luna.equals("Februarie") || luna.equals("februarie") && !((an % 4 == 0) && (an % 100 != 0) || (an % 400 == 0)) && zi<29)
+            System.out.println("Data introdusa este valida " + zi +luna+an);
+        else if (luna.equals("Aprilie") || luna.equals("aprilie") && zi == 31)
+            System.out.println("Data introdusa nu este valida");
+        else if (luna.equals("Aprilie") || luna.equals("aprilie") && zi < 31)
+            System.out.println("Data introdusa este valida " + zi +luna+an);
+        else if (luna.equals("Iunie") || luna.equals("iunie") && zi == 31)
+            System.out.println("Data introdusa nu este valida");
+        else if (luna.equals("Iunie") || luna.equals("iunie") && zi < 31)
+            System.out.println("Data introdusa este valida " + zi +luna+an);
+        else if (luna.equals("Septembrie") || luna.equals("septembrie") && zi == 31)
+            System.out.println("Data introdusa nu este valida");
+        else if (luna.equals("Septembrie")  || luna.equals("septembrie") && zi < 31)
+            System.out.println("Data introdusa este valida " + zi +luna+an);
+        else if (luna.equals("Noiembrie")  || luna.equals("noiembrie") && zi == 31)
+            System.out.println("Data introdusa nu este valida");
+        else if (luna.equals("Noiembrie") || luna.equals("noiembrie29") && zi < 31)
+            System.out.println("Data introdusa este valida " + zi +luna+an);
 
     }
 }
